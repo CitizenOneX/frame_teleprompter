2edc8a2cdd4ee2febee91f8fbbf7fea5ccd9885d
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-08-23 13:56:55 +1000

    added line-by-line pagination, vertical swipe gesture detection for page turning with a full-screen gesture input region (hence should work out of the box with tiktok scrolling rings)

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:file_picker/file_picker.dart';
 
 
 void main() {
-  runApp(const MaterialApp(home: TeleprompterPage()));
+  runApp(MaterialApp(
+    home: const TeleprompterPage(),
+    theme: ThemeData.dark(),
+  ));
 }
 
 class TeleprompterPage extends StatefulWidget {
@@ -15,7 +18,8 @@ class TeleprompterPage extends StatefulWidget {
 }
 
 class _TeleprompterPageState extends State<TeleprompterPage> {
-  String _fileContent = '';
+  List<String> _splitFile = [];
+  int _currentLine = -1;
 
   Future<void> _pickFile() async {
     try {
@@ -28,12 +32,14 @@ class _TeleprompterPageState extends State<TeleprompterPage> {
       if (result != null) {
         File file = File(result.files.single.path!);
 
-        // Read the file content
+        // Read the file content and split into lines
         String content = await file.readAsString();
+        _splitFile.clear();
 
         // Update the UI
         setState(() {
-          _fileContent = content;
+          _splitFile.addAll(content.split('\n'));
+          _currentLine = 0;
         });
       }
     } catch (e) {
@@ -47,26 +53,36 @@ class _TeleprompterPageState extends State<TeleprompterPage> {
       appBar: AppBar(
         title: const Text('Frame Teleprompter'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: _pickFile,
-              child: const Text('Pick Text File'),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Text(
-                  _fileContent,
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ),
-            ),
-          ],
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onVerticalDragEnd: (x) {
+            // x.velocity.pixelsPerSecond.dy reports about +4000 or -4000
+            // for Prev and Next buttons respectively
+            if (x.velocity.pixelsPerSecond.dy > 0) {
+              _currentLine > 0 ? --_currentLine : null;
+            }
+            else {
+              _currentLine < _splitFile.length - 1 ? ++_currentLine : null;
+            }
+            if (mounted) setState(() {});
+          },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Spacer(),
+              Text(
+                 _currentLine >= 0 ? _splitFile[_currentLine] : 'Load a file',
+                 style: const TextStyle(fontSize: 24),
+                                    ),
+              const Spacer(),
+            ],
+          ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(onPressed: _pickFile, child: const Icon(Icons.file_open)),
     );
   }
 }
